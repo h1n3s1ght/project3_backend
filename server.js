c543816894955d79ba5a7e5e7c008dab5d17fa31
h1n3s1ght <benjaminisaachines@gmail.com>
2022-06-23 09:01:50 -0500

    Update Put Path for show13

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get("/Landing", async (req, res) => {
 app.post("/TaskList/:_id", async (req, res) => {
     try {
         res.json(await List.create(req.body))
-        res.json(await List.findById(req.body._id))
+        res.json(await List.find({}))
     } catch(error) {
         res.status(400).json(error);
     }
